e6dd0e698d5dc1af59aa3fc443bb5fbcf7ea022b
masataka303 <65025509+masataka303@users.noreply.github.com>
2023-07-16 13:22:58 +0900

    Add files via upload

---
+++ psql.js
@@ -0,0 +1,23 @@
+const { Client } = require("pg");
+const client = new Client({
+  user: "admin",
+  host: "dpg-ciojm7tph6elhbvclmsg-a.singapore-postgres.render.com",
+  database: "lrs",
+  password: "hLNyXAdpuD9ZTww8KbkkzpraCZDI3Cnw",
+  port: 5432,
+});
+client.connect();
+/*const query = {
+  text: "SELECT * FROM user2",
+};
+client
+  .query(query)
+  .then((res) => {
+    console.log(res.rows[0]);
+    client.end();
+  })
+  .catch((e) => console.error(e.stack));*/
+  client.query("SELECT NOW()", (err, res) => {
+  console.log(err, res);
+  client.end();
+});
