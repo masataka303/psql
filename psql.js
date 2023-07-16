33a1ca73f81efd8fcb374102fa0681e686eee8af
masataka303 <65025509+masataka303@users.noreply.github.com>
2023-07-16 14:04:22 +0900

    Update psql.js

--- psql.js
+++ psql.js
@@ -1,7 +1,8 @@
 const { Client } = require("pg");
 const client = new Client({
   user: "admin",
-  host: "dpg-ciojm7tph6elhbvclmsg-a.singapore-postgres.render.com",
+  //host: "dpg-ciojm7tph6elhbvclmsg-a.singapore-postgres.render.com",
+  host: "dpg-ciojm7tph6elhbvclmsg-a",
   database: "lrs",
   password: "hLNyXAdpuD9ZTww8KbkkzpraCZDI3Cnw",
   port: 5432,
